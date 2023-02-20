c986cc02a5a1642cba5e756a2dad1f92cd1dae9e
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-20 11:14:39 +0900

    rename: 파일 이름 수정

---
+++ todo/src/styles/Wrapper.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  /* background: linear-gradient(to bottom right, #0cdbc9, #70bdf0); */
+  background: ${({ theme }) =>
+    `linear-gradient(to bottom right, ${theme.color["green-100"]}, ${theme.color["green-200"]})`};
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
