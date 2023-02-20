372cde01a61aded6e5eb4566458a3fa6793b8e7b
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-20 09:35:57 +0900

    chore: 필요없는 파일 삭제

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import AddItem from "./components/AddItem";
 import ItemList from "./components/ItemList";
 import { Item } from "./types/type";
